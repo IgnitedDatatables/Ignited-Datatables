f36ed7fcf1089c2a7df66c9b00eea76ebadb7cfa
Yusuf Özdemir <numberone.fatalrhymer@gmail.com>
2011-05-20 14:23:33 -0700

    - cleanup code

--- application/libraries/Datatables.php
+++ application/libraries/Datatables.php
@@ -356,13 +356,9 @@
           foreach($this->custom_columns as $cus_col_key => $cus_col_val)
           {
             if(isset($cus_col_val[1]) && is_array($cus_col_val[1]))
-            {
               foreach($cus_col_val[1] as $cus_colr_key => $cus_colr_val)
                 $cus_col_val[0] = str_ireplace('$' . ($cus_colr_key + 1), $aaData[$row_key][array_search($cus_colr_val, $this->referenced_columns)], $cus_col_val[0]);
 
-              $aaData[$row_key][] = $cus_col_val[0];
-            }
-            else
               $aaData[$row_key][] = $cus_col_val[0];
           }
         }
